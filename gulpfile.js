48a62c150b77744abc07d33b6685d2735f495e7f
Brian Mackey <mackey.brian@gmail.com>
2015-03-23 09:46:20 -0500

    added gulp-nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+nodemon = require('gulp-nodemon');
 
 gulp.task('default', function() {
   // place code for your default task here
 });
+
+gulp.task('start', function () {
+  nodemon({
+    script: 'app.js'
+  , ext: 'js html'
+  , env: { 'NODE_ENV': 'development' }
+  })
+})
